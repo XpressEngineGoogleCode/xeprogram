8642d426f3e7b820056c7c4a6432dbe1582da80b
dowon2308 <dowon2308@7e6db1ec-4dcf-11de-aa6a-4703752c21e7>
2009-06-02 09:30:42 +0000

    배팅 모듈 관리자 페이지에  설정 메뉴 추가

--- modules/bet/bet.admin.view.php
+++ modules/bet/bet.admin.view.php
@@ -24,6 +24,16 @@
          * @brief 배팅 관리자 페이지
          **/
         function dispBetAdminContent() {
+            // 스킨 목록을 구해옴
+            $oModuleModel = &getModel('module');
+            $skin_list = $oModuleModel->getSkins($this->module_path);
+            Context::set('skin_list',$skin_list);
+
+            // 레이아웃 목록을 구해옴
+            $oLayoutMode = &getModel('layout');
+            $layout_list = $oLayoutMode->getLayoutList();
+            Context::set('layout_list', $layout_list);
+
             // 템플릿에서 사용할 변수를 Context::set()
             if($this->module_srl) Context::set('module_srl',$this->module_srl);
             Context::set('module_info', $this->module_info);
