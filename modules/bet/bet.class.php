a42d2bd74373987b94cc1e5f4676bd9452918b18
dowon2308 <dowon2308@7e6db1ec-4dcf-11de-aa6a-4703752c21e7>
2009-06-03 08:35:56 +0000

    1. 설정 저장되게 수정 2. 권한 관리 메뉴 작동하게 수정

--- modules/bet/bet.class.php
+++ modules/bet/bet.class.php
@@ -11,20 +11,66 @@
          * @brief 설치시 추가 작업이 필요할시 구현
          **/
         function moduleInstall() {
-            return new Object();
+            $oModuleModel = &getModel('module');
+            $oModuleController = &getController('module');
+            $oBetController = &getController('bet');
+
+            $module_info = $oModuleModel->getModuleConfig('bet');
+            if($module_info->mid) {
+                $_o = executeQuery('module.getMidInfo', $module_info);
+                if(!$_o->data) unset($module_info);
+            }
+
+            if(!$module_info->mid) {
+                $args->module = 'bet';
+                $args->browser_title = 'betXE';
+                $args->skin = 'default';
+                $args->is_default = 'N';
+                $args->mid = 'bet';
+                $args->module_srl = getNextSequence();
+                $output = $oModuleController->insertModule($args);
+
+                $bet_args->mid = $args->mid;
+                $oBetController->insertConfig($bet_args);
+            }
         }
 
         /**
          * @brief 설치가 이상이 없는지 체크하는 method
          **/
         function checkUpdate() {
-            return false;
+            $oModuleModel = &getModel('module');
+            $oModuleController = &getController('module');
+            $oBetController = &getController('bet');
+
+            $module_info = $oModuleModel->getModuleConfig('bet');
+            if(!$module_info->mid) return true;
+
+			return false;
         }
 
         /**
          * @brief 업데이트 실행
          **/
         function moduleUpdate() {
+            $oModuleModel = &getModel('module');
+            $oModuleController = &getController('module');
+            $oBetController = &getController('bet');
+
+            $module_info = $oModuleModel->getModuleConfig('bet');
+            if(!$module_info->mid) {
+                $args->module = 'bet';
+                $args->browser_title = 'betXE';
+                $args->skin = 'default';
+                $args->is_default = 'N';
+                $args->mid = 'bet';
+                $args->module_srl = getNextSequence();
+                $output = $oModuleController->insertModule($args);
+
+                $bet_args->mid = $args->mid;
+                $oBetController->insertConfig($bet_args);
+            }
+
             return new Object(0,'success_updated');
         }
 
---
+++ modules/bet/bet.model.php
@@ -0,0 +1,43 @@
+<?php
+    /**
+     * @class  betModel
+     * @author SMaker (dowon2308@paran.com)
+     * @brief  bet 모듈의 Model class
+     **/
+
+    class betModel extends bet {
+
+        /**
+         * @brief 초기화
+         **/
+        function init() {
+        }
+
+        /**
+         * @brief 설정 return
+         **/
+        function getConfig() {
+            static $module_info = null;
+            if(is_null($module_info)) {
+                // module module_info의 값을 구함
+                $oModuleModel = &getModel('module');
+                $module_info = $oModuleModel->getModuleConfig('bet');
+
+                $skin_info->module_srl = $module_info->module_srl;
+                $oModuleModel->syncSkinInfoToModuleInfo($skin_info);
+
+                // bet dummy module의 is_default 값을 구함
+                $dummy = $oModuleModel->getModuleInfoByMid($module_info->mid);
+                $module_info->is_default = $dummy->is_default;
+                $module_info->module_srl = $dummy->module_srl;
+                $module_info->browser_title = $dummy->browser_title;
+                $module_info->layout_srl = $dummy->layout_srl;
+
+                if(count($skin_info)) foreach($skin_info as $key => $val) $module_info->{$key} = $val;
+
+                unset($module_info->grants);
+            }
+            return $module_info;
+        }
+    }
+?>
