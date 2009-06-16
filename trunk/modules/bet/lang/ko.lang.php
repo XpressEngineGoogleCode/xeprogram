c0d2168ebf030e08385c49ce45c0408338a85dbb
dowon2308 <dowon2308@7e6db1ec-4dcf-11de-aa6a-4703752c21e7>
2009-06-16 09:53:44 +0000

    [배팅 모듈] 스킨 수정 1. 배팅 가능한 시간 (24시간))이 지나면 배팅 불가능하게 수정 2. 배율을 목록에 표시 3. 누적 포인트를 관리 권한이 없는 사용자에게도 보이게 수정

--- trunk/modules/bet/lang/ko.lang.php
+++ trunk/modules/bet/lang/ko.lang.php
@@ -12,6 +12,7 @@
 	$lang->cumulative_point = '누적 포인트';
 	$lang->point = '포인트';
 	$lang->bet_point = '배팅할 포인트';
+	$lang->magnifications = '배율';
 
 	$lang->team1 = '상대';
 	$lang->team2 = '상대';
@@ -24,4 +25,5 @@
     $lang->msg_not_enough_point = '포인트가 부족합니다';
 	$lang->msg_input_score = '점수를 정확하게 입력해 주세요.';
 	$lang->msg_input_point = '배팅할 포인트를 정확하게 입력해 주세요.';
+	$lang->msg_cannot_beting = '배팅 가능한 시간이 지나 배팅할 수 없습니다.'
 ?>
